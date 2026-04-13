be9a0fea4f49b8fab18150f75250f8f591918281
Maciej Wlazło <carenauur@gmail.com>
2026-04-13 11:23:27 +0200

    project setup - init vite - structure

---
+++ src/app/App.tsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+
+function TreePage()
+{
+    return <div>TreePage</div>;
+}
+
+function NodeDetailsPage()
+{
+    return <div>NodeDetailsPage</div>;
+}
+
+export default function App()
+{
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/tree" element={<TreePage />} />
+            <Route path="/node/:id" element={<NodeDetailsPage />} />
+        </Routes>
+    );
+}
